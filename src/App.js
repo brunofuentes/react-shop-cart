3da19138c973c84b794e316692f633f04895575b
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-10-11 13:58:24 -0300

    add all routes and components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Home from './pages/Home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Basket from './pages/Basket'
+import PageLayout from './pages/PageLayout'
+import { ShopProvider } from './context/ShopContext'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<main>
+			<ShopProvider>
+				<Router>
+					<Routes>
+						<Route element={<PageLayout />}>
+							<Route path="/" element={<Home />} />
+							<Route path="/basket" element={<Basket />} />
+						</Route>
+					</Routes>
+				</Router>
+			</ShopProvider>
+		</main>
+	)
 }
 
-export default App;
+export default App
