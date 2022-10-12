5c485a91634b8daadadac85eafb8e9f60a566f78
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-10-12 19:53:46 -0300

    add function to change item-qty to ProductList component

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,28 +2,44 @@ import React, { useContext } from 'react'
 import ShopContext from '../context/ShopContext'
 
 function ProductList() {
-	const { shopItems } = useContext(ShopContext)
+	const { shopItems, addItemToCart, changeShopItemQty } = useContext(ShopContext)
+
 	return (
 		<>
 			<div className="p-3">
 				<table className="table-auto border">
-					<thead className="border-b">
-						<th className="border-r p-1">produto</th>
-						<th className="border-r p-1">descrição</th>
-						<th></th>
+					<thead>
+						<tr className="border-b">
+							<th className="border-r p-1">Produto</th>
+							<th className="border-r p-1">Descrição</th>
+							<th className="border-r p-1">Preço</th>
+						</tr>
 					</thead>
 					<tbody>
 						{shopItems.map((item, index) => (
-							<tr className="border-b">
+							<tr key={index} className="border-b">
 								<td className="border-r">{item.name}</td>
 								<td className="border-r">{item.description}</td>
+								<td className="border-r">R${item.price}</td>
 								<td>
 									<div className="flex">
 										<div className="border p-1">
-											<input type="number" placeholder="quantidade" />
+											<input
+												type="number"
+												min="1"
+												defaultValue={item.qty}
+												onChange={(e) => changeShopItemQty(item, parseInt(e.target.value))}
+											/>
 										</div>
 										<div className="border p-1">
-											<button className="px-10 bg-gray-400 rounded">adicionar</button>
+											<button
+												onClick={() => {
+													addItemToCart(item)
+												}}
+												className="px-10 bg-gray-400 rounded"
+											>
+												adicionar
+											</button>
 										</div>
 									</div>
 								</td>
