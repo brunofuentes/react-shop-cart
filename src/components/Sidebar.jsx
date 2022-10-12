ec6574f5b1b38ccb7eb3584aa23443b17c13bc41
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-10-12 19:41:17 -0300

    add conditional render based on path

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,27 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { useNavigate } from 'react-router'
 import ShopContext from '../context/ShopContext'
 
 function Sidebar() {
 	const { cartItems } = useContext(ShopContext)
+
+	let currentPath = window.location.pathname
 	let navigate = useNavigate()
+
 	return (
-		<div className="flex flex-col border-r w-1/6">
-			<button onClick={() => navigate('/')}>home</button>
-			<button onClick={() => navigate('/basket')}>cesta {cartItems.length > 0 && cartItems.length}</button>
-		</div>
+		<>
+			{!currentPath.includes('/admin') ? (
+				<div className="flex flex-col border-r w-1/6">
+					<button onClick={() => navigate('/')}>Home</button>
+					<button onClick={() => navigate('/basket')}>Cesta {cartItems.length > 0 && cartItems.length}</button>
+				</div>
+			) : (
+				<div className="flex flex-col border-r w-1/6">
+					<button onClick={() => navigate('/admin')}>Produtos</button>
+					<button onClick={() => navigate('/admin/orders')}>Compras</button>
+				</div>
+			)}
+		</>
 	)
 }
 
