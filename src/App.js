ce14b5adb6156b20ca45a89a7ca0c124ed5ca437
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-10-12 19:26:49 -0300

    update Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css'
 import Home from './pages/Home'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Basket from './pages/Basket'
+import Basket from './components/Basket'
 import PageLayout from './pages/PageLayout'
 import { ShopProvider } from './context/ShopContext'
+import Admin from './pages/Admin'
+import OrdersList from './components/OrdersList'
+import OrderComplete from './pages/OrderComplete'
 
 function App() {
 	return (
@@ -13,7 +16,11 @@ function App() {
 					<Routes>
 						<Route element={<PageLayout />}>
 							<Route path="/" element={<Home />} />
+							<Route path="*" element={<Home />} />
 							<Route path="/basket" element={<Basket />} />
+							<Route path="/checkout" element={<OrderComplete />} />
+							<Route path="/admin" element={<Admin />} />
+							<Route path="/admin/orders" element={<OrdersList />} />
 						</Route>
 					</Routes>
 				</Router>
