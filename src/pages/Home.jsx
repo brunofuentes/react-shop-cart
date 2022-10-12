521f76e3596c3231a46d08db3881d9aaa38ad132
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-10-12 19:33:41 -0300

    add updateShopItems and updateCartItems

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
 import ProductList from '../components/ProductList'
+import ShopContext from '../context/ShopContext'
 
 function Home() {
+	const { updateShopItems, updateCartItems } = useContext(ShopContext)
+
+	useEffect(() => {
+		updateShopItems()
+	}, [])
+
+	useEffect(() => {
+		updateCartItems()
+	})
+
 	return (
-		<>
-			<div className="flex">
-				<div className="flex-grow p-3">
-					<div className="flex justify-between">
-						<span>lugar do carrinho</span>
-						<span className="border items-center flex p-1 rounded">
-							<img className="inline" width={16} height={16} src="/images/icons/search_icon.svg" alt="Search product" />
-							<input type="text" placeholder="" />
-						</span>
-					</div>
-					<ProductList />
-				</div>
+		<div className="flex flex-grow flex-col">
+			<div className="flex justify-between p-3">
+				<span>Home</span>
+				<span className="border items-center flex p-1 rounded">
+					<img className="inline" width={16} height={16} src="/images/icons/search_icon.svg" alt="Search product" />
+					<input type="text" placeholder="" />
+				</span>
 			</div>
-		</>
+			<ProductList />
+		</div>
 	)
 }
 
