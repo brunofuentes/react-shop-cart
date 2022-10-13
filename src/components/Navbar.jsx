f5c1c74f4fc5f7be57bbdff99202ac1c84de72ae
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-10-13 16:11:01 -0300

    add icons to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,13 @@ import React from 'react'
 function Navbar() {
 	return (
 		<div className="flex justify-between border-b p-3">
-			<span>S-Shop</span>
-			<div>Hamburger Menu</div>
+			<a className="flex hover:scale-105" href="/">
+				<img src="/images/icons/shop_cart_icon.svg" width={20} height={20} alt="" />
+				<span className="pl-2 font-bold">S-Shop</span>
+			</a>
+			<span>
+				<img width={25} height={25} src="/images/icons/bars_icon.svg" alt="Menu" />
+			</span>
 		</div>
 	)
 }
