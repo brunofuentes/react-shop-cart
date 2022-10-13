22d40a146a19ecd4fe85a4bdd374657c3f98b558
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-10-13 16:11:12 -0300

    add style to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext } from 'react'
 import { useNavigate } from 'react-router'
 import ShopContext from '../context/ShopContext'
 
@@ -11,14 +11,22 @@ function Sidebar() {
 	return (
 		<>
 			{!currentPath.includes('/admin') ? (
-				<div className="flex flex-col border-r w-1/6">
-					<button onClick={() => navigate('/')}>Home</button>
-					<button onClick={() => navigate('/basket')}>Cesta {cartItems.length > 0 && cartItems.length}</button>
+				<div className="flex flex-col border-r w-1/6 py-3">
+					<button className="bg-slate-300 hover:scale-105 hover:bg-slate-400 shadow-sm" onClick={() => navigate('/')}>
+						Home
+					</button>
+					<button className="bg-slate-300 hover:scale-105 hover:bg-slate-400 shadow-sm" onClick={() => navigate('/basket')}>
+						Cesta {cartItems.length > 0 && cartItems.length}
+					</button>
 				</div>
 			) : (
-				<div className="flex flex-col border-r w-1/6">
-					<button onClick={() => navigate('/admin')}>Produtos</button>
-					<button onClick={() => navigate('/admin/orders')}>Compras</button>
+				<div className="flex flex-col border-r w-1/6 py-3">
+					<button className="bg-slate-300 hover:scale-105 hover:bg-slate-400 shadow-sm" onClick={() => navigate('/admin')}>
+						Produtos
+					</button>
+					<button className="bg-slate-300 hover:scale-105 hover:bg-slate-400 shadow-sm" onClick={() => navigate('/admin/orders')}>
+						Compras
+					</button>
 				</div>
 			)}
 		</>
