1030c3460206b9a6673dce4772bbccee66b41611
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-10-13 21:51:35 -0300

    translate placeholder to PT

--- src/components/SearchProduct.jsx
+++ src/components/SearchProduct.jsx
@@ -8,7 +8,7 @@ function SearchProduct() {
 		<div>
 			<span className="border items-center flex p-1 rounded">
 				<img className="inline" width={16} height={16} src="/images/icons/search_icon.svg" alt="Search product" />
-				<input className="pl-1" onChange={(e) => setKeyword(e.target.value)} value={keyword} type="text" placeholder="Search Product" />
+				<input className="pl-1" onChange={(e) => setKeyword(e.target.value)} value={keyword} type="text" placeholder="Procurar Produto" />
 			</span>
 		</div>
 	)
